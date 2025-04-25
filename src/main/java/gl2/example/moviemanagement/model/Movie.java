ce2911dd7dc31e4686dd1f05738dca4f70957a07
Ahmed Loubiri <ahmed.loubiri@insat.ucar.tn>
2025-04-25 14:36:38 +0100

    Movie entity

---
+++ src/main/java/gl2/example/moviemanagement/model/Movie.java
@@ -0,0 +1,56 @@
+package gl2.example.moviemanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String genre;
+    private String realisateur;
+
+    public Movie() {}
+
+    public Movie(String name, String position, String salary) {
+        this.name = name;
+        this.genre = position;
+        this.realisateur = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRealisateur() {
+        return realisateur;
+    }
+
+    public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+    }
+}
