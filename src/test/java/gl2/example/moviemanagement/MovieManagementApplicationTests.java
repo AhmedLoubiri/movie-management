b365861029537e3bb37135f5a61b7dd3fad31fc5
Ahmed Loubiri <ahmed.loubiri@insat.ucar.tn>
2025-04-25 14:09:51 +0100

    first commit

---
+++ src/test/java/gl2/example/moviemanagement/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package gl2.example.moviemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
