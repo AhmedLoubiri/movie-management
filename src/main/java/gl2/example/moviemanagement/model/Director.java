31d71335a80c2e3bc17131dbe4f42f5cf42fc1cf
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-04-25 15:12:58 +0100

    Director model

---
+++ src/main/java/gl2/example/moviemanagement/model/Director.java
@@ -0,0 +1,57 @@
+package gl2.example.moviemanagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Director {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private Integer age;
+
+  @Column(name = "image_url")
+  private String imageUrl;
+
+  public Director() {
+  }
+
+  public Director(String name, Integer age, String imageUrl) {
+    this.name = name;
+    this.age = age;
+    this.imageUrl = imageUrl;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+}
