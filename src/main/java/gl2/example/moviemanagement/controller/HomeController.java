6eba816dedb5de382f0252bd18f435bf4e142c77
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-05-05 19:55:20 +0100

    /api/test

--- src/main/java/gl2/example/moviemanagement/controller/HomeController.java
+++ src/main/java/gl2/example/moviemanagement/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @GetMapping("/")
+  @GetMapping({ "/", "/api/test", "/api/test/" })
   public String index() {
     return "redirect:/swagger-ui/index.html";
   }
